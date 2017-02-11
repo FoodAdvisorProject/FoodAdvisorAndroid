1d99d5023eab1ee58dc1622235c9871b4cf0a1f0
Andrea Lacava <thecave003@hotmail.it>
2017-02-11 15:50:03 +0100

    Added various activities
    Ho aggiunto parecchie nuove attività, il layout è pressocchè finito
    Bisogna ora iniziare a concentrarsi sulle API e sulla lettura del codice
    QR

---
+++ app/src/main/java/pro/rane/myapplication/RegisterActivity.java
@@ -0,0 +1,19 @@
+package pro.rane.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+
+    public void register(View view) {
+
+    }
+}
