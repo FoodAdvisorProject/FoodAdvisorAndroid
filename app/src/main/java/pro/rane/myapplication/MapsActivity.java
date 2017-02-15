e18c98cec9396e0885b24f87ed7270606f278172
Andrea Lacava <thecave003@hotmail.it>
2017-02-15 16:23:38 +0100

    Call to getarticletravel done
    Ho riscritto tutta la chiamata rest e la tokenizzazione. Ora la funzione
    dopo aver eseguito tutta la sua logica passa dell "dummy_coordinates" da
    eliminare nella versione finale.
    Conto di andare avanti nella implementazione.
    Ho inoltre rinserito la key google, non so chi la aveva levata

--- app/src/main/java/pro/rane/myapplication/MapsActivity.java
+++ app/src/main/java/pro/rane/myapplication/MapsActivity.java
@@ -1,9 +1,14 @@
 package pro.rane.myapplication;
 
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,19 +16,32 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String LATITUDE = "latitude";
+    private static final String LONGITUDE = "longitude";
+
     private GoogleMap mMap;
-   // private String info;
+    private String info;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +51,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         Bundle b = getIntent().getExtras();
-        if(b != null)
+        if (b != null)
             info = b.getString("qrCodeInformation");
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
@@ -47,58 +68,113 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
     /*connection to obtain the array  of positions*/
-    private static String[][] getCoordinates(){
-        String receive;
-        /*use string info for get request*/
-        // Create http client object to send request to server
-        HttpClient Client = new DefaultHttpClient();
-        // Create URL string
-        String URL = "qui va http://... seguito dalla richiesta con il codice" ;
-        //Log.i("httpget", URL);
-        try
-        {
-            String SetServerString;
+    private static String[][] getCoordinates(/*String tran_id  in attesa del lettoreQR*/) throws JSONException {
 
-            // Create Request to server and get response
+        String dummy_tran_id = "1";
 
-            HttpGet httpget = new HttpGet(URL);
-            ResponseHandler<String> responseHandler = new BasicResponseHandler();
-            SetServerString = Client.execute(httpget, responseHandler);
+        String richiesta = "http://foodadvisor.rane.pro:8080/getArticleTravel?tran_id=" + dummy_tran_id;
 
-            // Show response on activity
+        DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+        HttpGet request = new HttpGet(richiesta);
+        // Depends on your web service
+        request.setHeader("Content-type", "application/json");
 
-            receive=SetServerString;
+        InputStream inputStream = null;
+        String result = null;
+        try {
+            HttpResponse response = httpclient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            inputStream = entity.getContent();
+            // json is UTF-8 by default
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            result = sb.toString();
+        } catch (Exception e) {
+            // Oops
+        } finally {
+            try {
+                if (inputStream != null) inputStream.close();
+            } catch (Exception squish) {
+                Log.i(squish.getMessage(), squish.getMessage());
+            }
         }
-        catch(Exception ex)
-        {
-            Log.i("Fail!","Fail!");
+
+        JSONArray jObject;
+        String[][] coordinates;
+
+        jObject = new JSONArray(result);
+        String[] latitude = new String[jObject.length()];
+        String[] longitude = new String[jObject.length()];
+
+        for (int i = 0; i < jObject.length(); i++) {
+
+            latitude[i] = jObject.getJSONObject(i).getString(LATITUDE);
+            longitude[i] = jObject.getJSONObject(i).getString(LONGITUDE);
         }
-        /*conversion from receive to two dimensional array string*/
-        /*{latitudine, longitudine}*/
 
-        //TODO CONVERTIRE LA STRINGA, LA TOKENIZZAZIONE VA FATTA IN BASE ALLE RESPONSE AL SERVER, vedere GET /getArticleTravel
+        /* Decommentare ad implementazione finita
+        coordinates = new String[latitude.length][longitude.length];
 
-        //Esempio
-        String [][] coordinates={{"45.465454","9.186515999999983"},{"41.9027835","12.496365500000024"}};
+            for(int i =0; i < latitude.length;i++){
+                coordinates[i][0] = latitude[i];
+                coordinates[i][1] = longitude[i];
+            }
+            */
 
-        return coordinates;
+        String[][] dummy_coordinates = {{"45.465454", "9.186515999999983"}, {"41.9027835", "12.496365500000024"}};
+
+        //return coordinates;
+        return dummy_coordinates;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        String[][] coordinates=getCoordinates();
+        String[][] coordinates = new String[0][];
+        try {
+            coordinates = getCoordinates();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         Integer a;
         mMap = googleMap;
         mMap.addMarker(new MarkerOptions()
                 .position(new LatLng(Double.parseDouble(coordinates[0][0]), Double.parseDouble(coordinates[0][1])))
                 .title("Start point"));
-        for(a = 0; a < coordinates.length; a++){
+        for (a = 0; a < coordinates.length; a++) {
             mMap.addMarker(new MarkerOptions()
                     .position(new LatLng(Double.parseDouble(coordinates[a][0]), Double.parseDouble(coordinates[a][1])))
                     .title("Point " + a.toString()));
         }
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
