3ea1c66ded3bc02a9b04633409f20778ed6d50b0
Andrea Lacava <thecave003@hotmail.it>
2017-03-29 22:11:37 +0200

    Bug report completed and working

--- app/src/main/java/pro/rane/foodadvisor/ReportBugFragment.java
+++ app/src/main/java/pro/rane/foodadvisor/ReportBugFragment.java
@@ -11,50 +11,46 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-/**
- * Created by Andrea on 08/03/2017.
- */
+import java.util.HashMap;
+
 
 public class ReportBugFragment extends Fragment{
 
     private static final String email = "food.advisor.ita@gmail.com";
     private EditText body;
     private Button bug_button;
-
+    pro.rane.foodadvisor.SessionManager session;
     public ReportBugFragment() {
         // Required empty public constructor
     }
 
-
-
-
-    private void reportABug(View view){
-        Intent i = new Intent(Intent.ACTION_SEND);
-        i.setType("message/rfc822");
-        i.putExtra(Intent.EXTRA_EMAIL  , new String[]{email});
-        i.putExtra(Intent.EXTRA_SUBJECT, "[BUG REPORT] "+ Build.VERSION.SDK_INT);
-        i.putExtra(Intent.EXTRA_TEXT   , body.getText().toString());
-        try {
-            startActivity(Intent.createChooser(i, "Invio la mail..."));
-        } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(getActivity(), "Non ci sono servizi email installati.", Toast.LENGTH_SHORT).show();
-        }
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_bug_report, container, false);
-        body = (EditText) getActivity().findViewById(R.id.body_email_bug);
+        session = new pro.rane.foodadvisor.SessionManager(getActivity().getApplicationContext());
+
+        final HashMap<String, String> user = session.getUserDetails();
+
+        body = (EditText) rootView.findViewById(R.id.body_mail_bug);
         bug_button = (Button) rootView.findViewById(R.id.button_send_bug);
         bug_button.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                reportABug(v);
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("message/rfc822");
+                i.putExtra(Intent.EXTRA_EMAIL  , new String[]{email});
+                i.putExtra(Intent.EXTRA_SUBJECT, "[BUG REPORT] User: " + user.get(SessionManager.KEY_NAME));
+                i.putExtra(Intent.EXTRA_TEXT   , body.getText().toString()+"\n\n Version: "+ Build.VERSION.RELEASE +" API: "+ Build.VERSION.SDK_INT + "Model: " + Build.MODEL+" Marca: "+Build.MANUFACTURER);
+                try {
+                    startActivity(Intent.createChooser(i, getString(R.string.title_app_selector_name)));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(getActivity(), "Non ci sono servizi email installati.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
